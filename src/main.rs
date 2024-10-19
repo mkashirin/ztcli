4219a9e86acebbc226b11616b5774e834e883770
mkashirin <m.k4shirin@yandex.ru>
2024-10-19 21:58:12 +0300

    Update project name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use ztrs::FromEnv;
+use ztcli::FromEnv;
 
 #[tokio::main]
 async fn main() {
     let central_client = zerotier_central::Client::from_env();
     let one_client = zerotier_one::Client::from_env();
-    ztrs::cli(&central_client, &one_client).await;
+    ztcli::cli(&central_client, &one_client).await;
 }
